7fe2c1d263e919c8faad9cd249aba1377818509b
Kevin Barrett <kb@kevinbarrett.org>
2021-12-16 09:44:58 -0500

    2021: day 16 cleanup

--- 2021/lib/days/day16.ex
+++ 2021/lib/days/day16.ex
@@ -19,7 +19,7 @@ defmodule AdventOfCode.Days.Day16 do
     calculate(p)
   end
 
-  def packet(b) do
+  defp packet(b) do
     {version, b} = version(b)
     {type, b} = type(b)
 
@@ -43,13 +43,13 @@ defmodule AdventOfCode.Days.Day16 do
     end
   end
 
-  def bitwise_child_packets(b, packets \\ []) do
+  defp bitwise_child_packets(b, packets \\ []) do
     {p, rest} = packet(b)
     packets = [p | packets]
     if rest == <<>>, do: packets |> Enum.reverse(), else: bitwise_child_packets(rest, packets)
   end
 
-  def count_child_packets(b, target_count, packets \\ []) do
+  defp count_child_packets(b, target_count, packets \\ []) do
     {p, rest} = packet(b)
     packets = [p | packets]
 
@@ -58,19 +58,19 @@ defmodule AdventOfCode.Days.Day16 do
       else: count_child_packets(rest, target_count, packets)
   end
 
-  def version(<<v::3, rest::bitstring>>), do: {v, rest}
-  def type(<<t::3, rest::bitstring>>), do: {to_type(t), rest}
+  defp version(<<v::3, rest::bitstring>>), do: {v, rest}
+  defp type(<<t::3, rest::bitstring>>), do: {to_type(t), rest}
 
-  def to_type(0), do: :sum
-  def to_type(1), do: :product
-  def to_type(2), do: :minimum
-  def to_type(3), do: :maximum
-  def to_type(4), do: :literal
-  def to_type(5), do: :gt
-  def to_type(6), do: :lt
-  def to_type(7), do: :eq
+  defp to_type(0), do: :sum
+  defp to_type(1), do: :product
+  defp to_type(2), do: :minimum
+  defp to_type(3), do: :maximum
+  defp to_type(4), do: :literal
+  defp to_type(5), do: :gt
+  defp to_type(6), do: :lt
+  defp to_type(7), do: :eq
 
-  def value(<<c::1, v::4, rest::bitstring>>, segment_count \\ 1, current \\ <<>>) do
+  defp value(<<c::1, v::4, rest::bitstring>>, segment_count \\ 1, current \\ <<>>) do
     current = <<current::bitstring, v::4>>
 
     if c == 1,
@@ -78,8 +78,8 @@ defmodule AdventOfCode.Days.Day16 do
       else: {b_to_i(current, segment_count), rest}
   end
 
-  def child_length(<<t::1, len::15, rest::bitstring>>) when t == 0, do: {:bits, len, rest}
-  def child_length(<<t::1, len::11, rest::bitstring>>) when t == 1, do: {:count, len, rest}
+  defp child_length(<<t::1, len::15, rest::bitstring>>) when t == 0, do: {:bits, len, rest}
+  defp child_length(<<t::1, len::11, rest::bitstring>>) when t == 1, do: {:count, len, rest}
 
   defp b_to_i(a, segment_count) do
     <<i::size(segment_count)-unit(4)>> = a
@@ -87,10 +87,7 @@ defmodule AdventOfCode.Days.Day16 do
   end
 
   defp sum_versions(packet) do
-    case packet.type do
-      :literal -> packet.version
-      _ -> packet.version + (packet.children |> Enum.map(&sum_versions(&1)) |> Enum.sum())
-    end
+    packet.version + (packet.children |> Enum.map(&sum_versions(&1)) |> Enum.sum())
   end
 
   defp calculate(packet) do
